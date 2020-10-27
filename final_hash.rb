eaa4b5bd36c0c0de1f8b2763133a1fe9ba0c17fa
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-27 21:41:40 +0000

    Automatically backed up by Learn

--- final_hash.rb
+++ final_hash.rb
@@ -5,7 +5,8 @@ monopoly =  {
   :railroads=>
   {
     :pieces=>4,
-    :rent_in_dollars=>
+    :rent_in_dollars=> {},
+    :names => {}
     {
       :one_piece_owned=>25,
       :two_pieces_owned=>50,
@@ -18,7 +19,7 @@ monopoly =  {
       {
         "mortgage_value"=>"$100"
       },
-      :pennsylvania_railroad=>
+      :pennsylvania_railroad=> {}
       {
         "mortgage_value"=>"$200"
       },
@@ -29,8 +30,8 @@ monopoly =  {
       :shortline=>
       {
         "mortgage_value"=>"$800"
+      },
       }
     }
   }
 }
-
