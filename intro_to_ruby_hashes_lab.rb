aa6c98122adb7444e80746a2215a31677618c5d4
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-27 21:57:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,6 +14,10 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 
+  hash = {
+    :railroads => {}
+  }
+
   hash[:railroads] = {
     :pieces => 4,
     :rent_in_dollars => {},
