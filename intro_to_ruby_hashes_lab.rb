37123bff8896f17fba468804b58defec851ac587
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-27 21:50:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,8 @@ def monopoly_with_third_tier
   hash[:railroads][:names] = {
     :reading_railroad => {},
     :pennsylvania_railroad => {},
-    :b_and_o_railroad => {}
+    :b_and_o_railroad => {},
+    :shortline_railroad => {}
   }
 
 end
