206b9d511decbe20f348ca9d0ac233b3b70c46b8
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-27 21:47:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,6 +27,7 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  hash[:railroads][:rent_in_dollars]
 
 end
 
