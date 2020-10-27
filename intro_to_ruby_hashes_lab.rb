d86b6640e0c7ad89442aa47b13105c5140e27dd9
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-27 22:04:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,18 +36,31 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+
+  hash = {
+    :railroads => {
+      :pieces => 4,
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200
+      },
+      :names => {
+        :reading_railroad => {},
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
+        :shortline_railroad => {}
+      }
+    }
+  }
+
   hash[:railroads][:rent_in_dollars] = {
-    :one_piece_owned => 25,
-    :two_pieces_owned => 50,
-    :three_pieces_owned => 100,
-    :four_pieces_owned => 200
+
   }
 
   hash[:railroads][:names] = {
-    :reading_railroad => {},
-    :pennsylvania_railroad => {},
-    :b_and_o_railroad => {},
-    :shortline_railroad => {}
+
   }
 
 end
