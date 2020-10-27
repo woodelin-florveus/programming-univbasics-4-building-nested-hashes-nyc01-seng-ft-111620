82a573c6b40a3e9f7017bd9a562b01d6269137cf
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-27 21:53:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,7 +52,7 @@ def monopoly_with_fourth_tier
   hash[:railroads][:names] = {
     :reading_railroad => {:mortgage_value => 100},
     :pennsylvania_railroad => {:mortgage_value => 200},
-    :b_and_o_railroad => {},
+    :b_and_o_railroad => {:mortgage_value => 400},
     :shortline_railroad => {}
   }
 
